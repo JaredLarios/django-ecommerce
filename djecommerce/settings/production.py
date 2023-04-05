49937266428e021e7ca84bea063709276a72a962
JaredLarios <lar21038@byui.edu>
2023-04-05 10:10:19 -0600

    fix: production

--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
-DEBUG = config('DEBUG', cast=bool)
-ALLOWED_HOSTS = ['ip-address', 'www.your-website.com']
+DEBUG = False
+ALLOWED_HOSTS = ['localhost', '192.168.100.103']
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
@@ -23,3 +23,11 @@ DATABASES = {
 
 STRIPE_PUBLIC_KEY = config('STRIPE_LIVE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_LIVE_SECRET_KEY')
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_SECONDS = 3153600 
