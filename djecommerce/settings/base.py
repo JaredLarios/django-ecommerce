5a556a249c6c6463bb3b5e5c08e741e7cae9a63b
JaredLarios <lar21038@byui.edu>
2023-04-04 22:52:06 -0600

    fix: deploy secret key

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -4,7 +4,7 @@ from decouple import config
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
